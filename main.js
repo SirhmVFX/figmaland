aca5c5677e5ed9fcefe689c3700496ca91280a75
sirhmvfx <sirhmvfx@gmail.com>
2023-02-06 10:42:36 +0100

    working on toggle feature

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav__elements"); 
+const menu = document.querySelector(".nav__elements");
+
+let toggleMode = document.getElementById("toggleMode");
 
 
 hamburger.onclick = function () {
     menu.classList.toggle("show");
 }
+
+toggleMode.addEventListener("click", function() {
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+})
